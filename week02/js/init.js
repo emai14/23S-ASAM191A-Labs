f3d3c16165335075591744a08baa3373490de595
em14 <ELSAMAI14@G.UCLA.EDU>
2023-04-19 17:45:58 -0700

    test

--- week02/js/init.js
+++ week02/js/init.js
@@ -1,7 +1,7 @@
 console.log("Hello Asia-Am 191A! :)")
 
 // JavaScript const variable declaration
-const map = L.map('the_map').setView([34.0709, -118.444], 14); 
+const map = L.map('the_map').setView([33.9700, -118.444], 10); 
 
 // Leaflet tile layer, i.e. the base map
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -9,14 +9,23 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map); 
 
 //JavaScript let variable declaration to create a marker
-let cookie = L.marker([34.0631, -118.4469]).addTo(map) 
-        .bindPopup('Diddy Riese!')
+let powell = L.marker([34.0716, -118.4422]).addTo(map) 
+        .bindPopup('My favorite place to study :)')
 
-let ackerman = L.marker([34.0705, -118.4442]).addTo(map)
-        .bindPopup('Ackerman Union')
+let smpier = L.marker([34.0083, -118.4988]).addTo(map)
+        .bindPopup('Arcade on Santa Monica Pier')
 
-function addMarker(lat,lng,message){ 
+let aotp = L.marker([33.7620,-118.1970]).addTo(map)
+        .bindPopup('Best aquarium ever :D')
+
+let ghost = L.marker([33.922428, -118.112282]).addTo(map)
+        .bindPopup('Where I adopted my dog <3')
+
+function addMarker(lat, lng, message){ 
     console.log(message) 
     L.marker([lat,lng]).addTo(map).bindPopup(message) 
     return message 
         }
+
+let tfc = L.marker([34.050450, -118.438120]).addTo(map)
+.bindPopup('Best Westwood restaurant')
